678406e6043d741e84f628a74c8206233b75d44e
Joe Berria <joeberria@gmail.com>
2018-12-14 20:40:59 -0800

    update to example main func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/nexes/goIRC/pkg/irc"
 )
 
 func main() {
-	fmt.Println("hello")
+	client := irc.NewClient("hilljoh", "", "irc.freenode.net")
+
+	client.HandleEventFunc(irc.EventConnect, func(event irc.EventType) {
+		fmt.Println("connection connect happened")
+	})
+
+	client.HandleEventFunc(irc.EventDisconnect, func(event irc.EventType) {
+		fmt.Println("disconnect event happened")
+	})
+
+	client.HandleEventFunc(irc.EventPing, func(event irc.EventType) {
+		fmt.Println("ping event happened ", event.Message)
+	})
+
+	client.HandleEventFunc(irc.EventError, func(event irc.EventType) {
+		fmt.Printf("Error event happened %v\n", event.Err)
+	})
+
+	// capture user input
+	go func() {
+	loop:
+		for {
+			var input string
+
+			_, err := fmt.Scanln(&input)
+			if err != nil {
+				fmt.Println("error reading stdio ", err)
+			}
+
+			switch strings.ToLower(input) {
+			case "/quit":
+				client.StopConnection()
+				break loop
+			}
+		}
+	}()
+
+	client.StartConnection()
+	fmt.Println("at the end of main")
 }
